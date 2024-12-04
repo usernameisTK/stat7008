2084eb108431b42fc45e9921a30274f18e6180f4
JcMew <163726315+JcMew@users.noreply.github.com>
2024-12-04 13:36:32 +0800

    Update function2.py
    ESG Topic

---
+++ function2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def get_topic_by_filename(fn):
+    file_path='ESG_Report_Topics.xlsx'
+    df = pd.read_excel(file_path)
+
+    matching_row = df[df['File Name'] == fn]
+
+    if not matching_row.empty:
+        return matching_row['Topic'].values[0]
+    else:
+        return "No matching topic found."
+    
+    
